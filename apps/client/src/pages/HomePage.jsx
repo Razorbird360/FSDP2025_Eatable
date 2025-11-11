5b53fc0964bc7263f27847ffe46c32cd980db57d
Ranen Sim <ranen.rzb@gmail.com>
2025-11-11 08:00:17 +0800

    chore: scale up hero advertisement

--- apps/client/src/pages/HomePage.jsx
+++ apps/client/src/pages/HomePage.jsx
@@ -11,29 +11,29 @@ function HomePage() {
       </div>
 
       <div
-        className="w-80 h-80 rounded-xl bg-gray-200 bg-cover bg-center relative"
+        className="w-96 h-96 rounded-xl bg-gray-200 bg-cover bg-center relative"
         style={{ backgroundImage: `url(${advertiseHero})` }}
         aria-label="Hawker food collage">
           <span className='absolute inset-0 w-full h-full bg-gradient-to-b from-transparent via-black/30 to-black/70 rounded-xl z-0'></span>
-          <div className="flex justify-between w-full p-3 px-4 items-center relative z-10">
-            <div className='w-32 h-8 bg-brand rounded-full flex justify-evenly p-0 items-center text-white'>
-              <img src={rating} alt="Rating" />
-              <p className="-translate-x-1 text-sm">Top-Rated</p>
+          <div className="flex justify-between w-full p-4 px-5 items-center relative z-10">
+            <div className='w-32 h-9 bg-brand rounded-full flex justify-evenly p-0 items-center text-white'>
+              <img src={rating} alt="Rating" className="w-5 h-5" />
+              <p className="-translate-x-1 text-base">Top-Rated</p>
             </div>
-            <div className='bg-white w-8 h-8 rounded-full flex items-center justify-center p-1'>
-              <img src={favourite} alt="Favourite" className="w-5 h-5" />
+            <div className='bg-white w-10 h-10 rounded-full flex items-center justify-center p-1'>
+              <img src={favourite} alt="Favourite" className="w-6 h-6" />
             </div>
           </div>
 
-          <div className='text-white w-full flex flex-col p-3 px-4 absolute bottom-0 mb-1'>
-            <p className="text-xl">Grilled Beef Fried Rice</p>
-            <p className="-translate-y-1">John&apos;s famous steaks</p>
+          <div className='text-white w-full flex flex-col p-4 px-5 absolute bottom-0 mb-1'>
+            <p className="text-2xl">Grilled Beef Fried Rice</p>
+            <p className="-translate-y-1 text-base">John&apos;s famous steaks</p>
             <div className='flex justify-between'>
-              <div className='bg-brand w-60 h-10 rounded-full flex justify-center items-center'>
-                <p className='font-bold text-sm'>Order now</p>
+              <div className='bg-brand w-72 h-12 rounded-full flex justify-center items-center'>
+                <p className='font-bold text-base'>Order now</p>
               </div>
-              <div className='bg-white/30 h-10 w-10 rounded-full flex items-center justify-center'>
-                <img src={location} alt="Location" className="w-6 h-6" />
+              <div className='bg-white/30 h-12 w-12 rounded-full flex items-center justify-center'>
+                <img src={location} alt="Location" className="w-7 h-7" />
               </div>
             </div>
 
