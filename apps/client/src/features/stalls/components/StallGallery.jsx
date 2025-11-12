1080ffa1ed75afadb3014cd2f9c8aa7de66e9651
MarcusOngJR <s10269545@connect.np.edu.sg>
2025-11-12 21:39:18 +0800

    minor fix: Lint error resolved

--- apps/client/src/features/stalls/components/StallGallery.jsx
+++ apps/client/src/features/stalls/components/StallGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import  { useMemo, useState, useEffect } from "react";
 
 export default function CustomBentoGrid() {
   // ===== Simulate logged-in user =====
